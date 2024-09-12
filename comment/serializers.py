3740d0c3a9ca7ac7a9144338ce86b1f23e151704
allan zizinga <allanzizinga@yahoo.com>
2024-09-13 00:50:22 +0200

    Added pagination to project

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -7,16 +8,30 @@ class CommentSeializer(serializers.ModelSerializer):
     Serializer for the Comment model, adds three extra fields when returning a list of Comment instances.
     """
     owner= serializers.ReadOnlyField(source='owner.username')
+    is_owner =serializers.SerializerMethodField()
+    profile_id =serializers.ReadOnlyField(source='owner.bio.id')
+    profile_image=serializers.ReadOnlyField(source='owner.bio.image.url')
+    created_at =serializers.SerializerMethodField()
+    updated_at =serializers.SerializerMethodField()
 
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
     
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+       
+    
+
+    
     class Meta:
         model= Comment
         fields= [
-           'id', 'owner','broadcast','created_at','updated_at','content'
+           'id', 'owner','broadcast','created_at','updated_at','content','is_owner','profile_id','profile_image','created_at','updated_at',
         ]
 
 class CommentDetailSerializer(CommentSeializer):
