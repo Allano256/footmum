e04c7008c01395f50b3ef9f0938766c583e5c5d2
allan zizinga <allanzizinga@yahoo.com>
2024-09-14 23:33:28 +0200

    Changed Procfile to footmum.wsgi

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -77,7 +77,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-     'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     'cloudinary_storage',
 
